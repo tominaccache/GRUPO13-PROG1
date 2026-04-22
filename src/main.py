ad553973cc8d0f0db988c3d3b96cd6bd46c262bc
Matute0512 <matiast999@hotmail.com>
2026-04-21 22:57:57 -0300

    "Modificacion eb el menu"

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from rich.console import Console
 from rich.panel import Panel
 from Tabla_Posiciones import menu_tabla_posiciones
+
 # Inicializamos la consola
 console = Console()
 
@@ -19,25 +20,25 @@ def mostrar_menu():
     """
     # Armamos el texto del menú con etiquetas de colores
     texto_menu = (
-    "[bold red]1. Gestionar Pilotos[/bold red]\n"
-    "[bold red]2. Gestionar Escuderías[/bold red]\n"
-    "[bold red]3. Registrar Resultados de Gran Premio[/bold red]\n"
-    "[bold red]4. Ver Tabla de Posiciones[/bold red]\n"
-    "[bold red]5. Ver Estadísticas[/bold red]\n"
-    "[bold red]6. Proyección de Campeonato[/bold red]\n"
-    "[bold red]7. Guardar Datos[/bold red]\n"
-    "[bold red]8. Cargar Datos[/bold red]\n"
-    "[bold red]0. Salir[/bold red]"
+        "[bold red]1. Gestionar Pilotos[/bold red]\n"
+        "[bold red]2. Gestionar Escuderías[/bold red]\n"
+        "[bold red]3. Registrar Resultados de Gran Premio[/bold red]\n"
+        "[bold red]4. Ver Tabla de Posiciones[/bold red]\n"
+        "[bold red]5. Ver Estadísticas[/bold red]\n"
+        "[bold red]6. Proyección de Campeonato[/bold red]\n"
+        "[bold red]7. Guardar Datos[/bold red]\n"
+        "[bold red]8. Cargar Datos[/bold red]\n"
+        "[bold red]0. Salir[/bold red]"
     )
 
     # Imprimimos el menú dentro de un recuadro
     panel = Panel(
-    texto_menu,
-    title="[bold red] Administrador de Campeonato F1 🏆[/bold red]",
-    border_style="bold red",
-    style="on white",
-    padding=(1, 4),
-    expand=False,
+        texto_menu,
+        title="[bold red] Administrador de Campeonato F1 🏆[/bold red]",
+        border_style="bold red",
+        style="on white",
+        padding=(1, 4),
+        expand=False,
     )
     console.print(panel)
 
@@ -66,24 +67,25 @@ def main():
                 console.print("[bold red]--> Generando Estadisticas[/bold red]")
             case "6":
                 console.print(
-            "[bold red]--> Generando Proyeccion del Campeonato[/bold red]"
-            )
+                    "[bold red]--> Generando Proyeccion del Campeonato[/bold red]"
+                )
             case "7":
                 console.print("[bold red]--> Guardando datos en Archivo[/bold red]")
             case "8":
                 console.print("[bold red]--> Cargando datos en Archivo[/bold red]")
             case "0":
                 console.print(
-            "[bold red]--> ¡Gracias por utilizar la aplicacion! Nos vemos la proxima.[/bold red]"
-            )
+                    "[bold red]--> ¡Gracias por utilizar la aplicacion! Nos vemos la proxima.[/bold red]"
+                )
             case _:
                 console.print(
-            "[bold red]--> Error: Opción no válida. Ingrese un número del 0 al 8.[/bold red]"
-            )
+                    "[bold red]--> Error: Opción no válida. Ingrese un número del 0 al 8.[/bold red]"
+                )
 
         if opcion != "0":
             print()
             console.input("[bold red]Presione Enter para continuar... [/bold red]")
+
 
 if __name__ == "__main__":
     main()
