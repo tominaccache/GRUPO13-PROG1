c4c667d97da5bb555f163742e870c4e42e438952
Matute0512 <matiast999@hotmail.com>
2026-04-22 21:15:50 -0300

    Modificacion de la logica

--- src/Tabla_Posiciones.py
+++ src/Tabla_Posiciones.py
@@ -9,31 +9,44 @@ def limpiar_consola():
     console.clear()
 
 
-def menu_tabla_posiciones():
-    while True:
-        limpiar_consola()
-        texto_menu = (
+def mostrar_menu_tabla_posiciones():
+  
+    texto_menu = (
             "[bold red]1. Ver Tabla de Pilotos[/bold red]\n"
             "[bold red]2. Ver Tabla de Escuderías[/bold red]\n"
             "[bold red]3. Exportar Clasificación[/bold red]\n"
             "[bold red]0. Volver al menú principal[/bold red]"
-        )
-
-        panel = Panel(
+    )
+    panel = Panel(
             texto_menu,
-            title="[bold red] Tabla de Posiciones 📊 [/bold red]",
+            title="[bold red] Tabla de Posiciones[/bold red]",
             border_style="bold red",
             style="on white",
             padding=(1, 4),
             expand=False,
-        )
-        console.print(panel)
+            width=49
+    )
+    console.print(panel)
+    return console.input("\n[bold red]Seleccione una opción: [/bold red]")
+        
+def menu_tabla_posiciones():
+    continuar_programa = True
+    while continuar_programa:
+        limpiar_consola()      
+        op=mostrar_menu_tabla_posiciones()
 
-        op = console.input("\n[bold red]Seleccione una opción: [/bold red]")
-        if op == "1":
-            limpiar_consola()
-            console.input("\nPresione Enter para volver...")
-
-        elif op == "0":
-            break
-
+        match op:
+            case "1":
+                console.print("[bold red]--> Ver Tabla de Pilotos (próximamente)[/bold red]")
+            case "2":
+                console.print("[bold red]--> Ver Tabla de Escuderías (próximamente)[/bold red]")
+            case "3":
+                console.print("[bold red]--> Exportar Clasificación (proximamente)[/bold red]")
+            case "0":
+                continuar_programa= False
+            case _:
+                console.print("[bold red]--> Opcion no Válida.[/bold red]")
+            
+        if op != "0":
+            console.input("\n[bold red]Presione Enter para continuar.[/bold red]")
+                    
