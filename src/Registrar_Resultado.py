b27ca5c0374b0b676f2a159cad5a1cd6b9442f27
Matute0512 <matiast999@hotmail.com>
2026-05-08 13:42:49 -0300

    Formateo de codigo

--- src/Registrar_Resultado.py
+++ src/Registrar_Resultado.py
@@ -1,29 +1,45 @@
 from rich.console import Console
-from rich.panel import Panel
-from rich.table import Table
 from utils import mostrar_menu_generico
+
 console = Console()
+
 
 def limpiar_consola():
     console.clear()
-    
+
+
 def registrar_tiempos():
     return
+
+
 def ver_resultados():
     return
+
+
 def modificar_resultados():
     return
+
+
 def eliminar_resultados():
     return
 
+
 def menu_resultados():
     continuar_programa = True
-    
-    opciones_submenu = ["1. Registrar tiempos de carrera","2. Ver resultados","3. Modificar Resultados","4. Eliminar resultados","0. Volver al menú principal"]
-    
+
+    opciones_submenu = [
+        "1. Registrar tiempos de carrera",
+        "2. Ver resultados",
+        "3. Modificar Resultados",
+        "4. Eliminar resultados",
+        "0. Volver al menú principal",
+    ]
+
     while continuar_programa:
         limpiar_consola()
-        opcion = mostrar_menu_generico("Registro de Resultados de Gran Premio",opciones_submenu)
+        opcion = mostrar_menu_generico(
+            "Registro de Resultados de Gran Premio", opciones_submenu
+        )
         match opcion:
             case "1":
                 registrar_tiempos()
@@ -36,7 +52,7 @@ def menu_resultados():
             case "0":
                 console.print("[bold red]--> Volviendo al menú principal...[/bold red]")
                 continuar_programa = False
-            case _: 
-                 console.print("[bold red]--> Opción no válida.[/bold red]")
+            case _:
+                console.print("[bold red]--> Opción no válida.[/bold red]")
         if opcion != "0":
-            console.input("\n[bold red]Presione enter para continuar.[/bold red]")    
+            console.input("\n[bold red]Presione enter para continuar.[/bold red]")
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 from rich.console import Console
 from rich.panel import Panel
-
+from rich.table import Table
 console = Console()
 
 def mostrar_menu_generico(titulo, opciones,ancho = 49):
@@ -31,5 +31,34 @@ def mostrar_menu_generico(titulo, opciones,ancho = 49):
     
     console.print(panel)
     return console.input("\n[bold red]Seleccione una opcion: [/bold red]")
+
+def mostrar_tabla_generica(titulo,filas,columnas):
+    """
+    Objetivo: Renderizar cualquier menú del sistema de forma dinámica.
+    Entradas:
+        - titulo (str): El título que aparecerá en el borde del panel.
+        - opciones (list): Lista de strings con las opciones a mostrar.
+        - ancho (int): Ancho del panel (por defecto 49, como usabas).
+    Salida: Retorna la opción ingresada por el usuario como string.
+    """
+    # Creamos la tabla con el titulo
+    tabla = Table(
+        title=f"{titulo}",
+        border_style="bold red",
+        style="on white"
+    )
     
+    # Agregamos las columnas dinamicamente
+    for col in columnas:
+        tabla.add_column(col,style="bold white",justify="center")
+        
+    # Agregamos las filas
+    for fila in filas:
+        # Convertimos cada dato de la fila a string por si hay numero 
+        fila_str = [str[dato] for dato in fila]
+        # El asterisco (*) desempaqueta la lista para que add_row lo entienda
+        tabla.add_row(*fila_str)
+        
+    console.print(tabla)
+        
     
