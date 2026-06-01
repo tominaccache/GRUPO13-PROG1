c8df863924e46800572ae162266af5b177cdd69a
NtobonH <Ntobonhenao@uade.edu.ar>
2026-06-01 14:39:47 -0300

    esqueleto base del submenu y control de flujo

--- src/subMenuEstadisticas.py
+++ src/subMenuEstadisticas.py
@@ -1,47 +1,54 @@
 from rich.console import Console
 from rich.panel import Panel
 
-#Inicializamos la consola
-
+# Inicializamos la consola
 console = Console()
 
 def mostrar_menu_estadisticas():
-    texto_menu= (
-        "[bold red]1. Pilotos con puntos[/bold red]\n"
+    """
+    Objetivo: Mostrar el panel visual con las opciones de estadísticas.
+    """
+    texto_menu = (
+        "[bold red]1. Piloto con puntos[/bold red]\n"
         "[bold red]2. Promedio de tiempos[/bold red]\n"
-        "[bold red]3. Mejor tiempo[/bold red]\n"
+        "[bold red]3. Mejor Tiempo[/bold red]\n"
         "[bold red]4. Cantidad de Victorias[/bold red]\n"
-        "[bold red]0. Volver al menú principal[/bold red]\n"
+        "[bold red]0. Salir[/bold red]\n"
     )
     panel = Panel(
         texto_menu,
         title="[bold red]Estadísticas[/bold red]",
         border_style="bold red",
         style="on white",
-        padding=(1,4),
+        padding=(1, 4),
         expand=False,
         width=49
     )
     console.print(panel)
-    
+
+
 def menu_estadisticas():
-    opcion ="-1"
+    """
+    Objetivo: Controlar el flujo y navegación del submenú.
+    """
+    opcion = "-1"
     while opcion != "0":
         console.clear()
         mostrar_menu_estadisticas()
         opcion = console.input("\n[bold red]Seleccione una opción: [/bold red]")
         match opcion:
             case "1":
-                console.print("[bold red]--> Pilotos con puntos (en producción)[/bold red]")
+                console.print("[bold yellow]--> Opción 1: Pilotos con puntos (En desarrollo)...[/bold yellow]")
             case "2":
-                console.print("[bold red]--> Promedio de tiempos (en producción)[/bold red]")
+                console.print("[bold yellow]--> Opción 2: Promedio de tiempos (En desarrollo)...[/bold yellow]")
             case "3":
-                console.print("[bold red]--> Mejor tiempo (en producción)[/bold red]")
+                console.print("[bold yellow]--> Opción 3: Mejor tiempo (En desarrollo)...[/bold yellow]")
             case "4":
-                console.print("[bold red]--> Cantidad de victorias (en producción)[/bold red]")
+                console.print("[bold yellow]--> Opción 4: Cantidad de victorias (En desarrollo)...[/bold yellow]")
             case "0":
                 console.print("[bold red]--> Volviendo al menú principal. [/bold red]")
             case _:
                 console.print("[bold red]--> Opción no válida.[/bold red]")
+                
         if opcion != "0":
-            console.input("[bold red]--> Presione Enter para continuar. [/bold red]")
+            console.input("\n[bold red]--> Presione Enter para continuar. [/bold red]")
